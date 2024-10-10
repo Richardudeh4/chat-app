9dd2998b073e2f7a632318ef6eadb6a633c3dac8
richardudeh4 <richardudeh03@gmail.com>
2024-10-10 06:22:49 +0100

    connected users to stream io

--- app/providers/authProvider.tsx
+++ app/providers/authProvider.tsx
@@ -5,14 +5,16 @@ import { supabase } from "../lib/supabase";
 type AuthContext ={
     session : Session | null ;
     user: User | null;
+    profile: any | null;
 }
 const AuthContext = createContext<AuthContext>({
     session: null,
     user: null,
+    profile: null,
 });
 export default function AuthProvider ({children}: PropsWithChildren) {
     const [session, setSession] = useState<Session | null>(null)
-
+    const [profile, setProfile] = useState();
     useEffect(() => {
       supabase.auth.getSession().then(({ data: { session } }) => {
         setSession(session)
@@ -21,9 +23,26 @@ export default function AuthProvider ({children}: PropsWithChildren) {
       supabase.auth.onAuthStateChange((_event, session) => {
         setSession(session)
       })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+      if(!session?.user){
+        setProfile(null);
+        return ;
+      }
+      const fetchProfile = async () => {
+        let {data, error } = await supabase
+        .from('profiles')
+        .select('*')
+        .eq('id', session.user.id)
+        .single();
+        setProfile(data)
+      }
+      fetchProfile();
+    }, [session?.user])
+    console.log(profile)
     return (
-        <AuthContext.Provider value={{session, user: session?.user}}>
+        <AuthContext.Provider value={{session, user: session?.user, profile}}>
         {children}
         </AuthContext.Provider>
     )
